df558bb6a97710c372821036d7fb02263a9defb4
unknown <[ismayilia@code.edu.az]>
2023-09-29 17:40:39 -0700

    added navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -34,14 +34,57 @@ $(function () {
 
 
 
+    let sideBar = document.querySelector(".icon-logo .transit")
 
+    let iconBasket = document.querySelector(".icon-logo .fa-bars")
+    
+    let iconClose = document.querySelector(".side-bar .icon-close")
+    
+    let overlay = document.querySelector(".icon-logo .overlay")
+    
+    
+    
+    iconBasket.addEventListener("click",function(){
+        sideBar.classList.remove("transit")
+        overlay.classList.remove("d-none")
+        
+    })
+    
+    iconClose.addEventListener("click",function(){
+        sideBar.classList.add("transit")
+        overlay.classList.add("d-none")
+    })
+    
+    overlay.addEventListener("click",function(){
+        sideBar.classList.add("transit")
+        overlay.classList.add("d-none")
+    })
 
+//Search
 
+let iconSearch =document.querySelector(".icons .search-icon .search")
+let searchPage = document.querySelector(".search-page")
+let iconClosePage = document.querySelector(".icons .search-icon .search-close")
 
+iconSearch.addEventListener("click",function(){
+    searchPage.classList.remove("d-none")
+    iconSearch.classList.add("d-none")
+    iconClosePage.classList.remove("d-none")
+})
 
+iconClosePage.addEventListener("click",function(){
+    searchPage.classList.add("d-none")
+    iconSearch.classList.remove("d-none")
+    iconClosePage.classList.add("d-none")
+})
 
+let contentCloseIcon = document.querySelector(".content i")
 
-
+contentCloseIcon.addEventListener("click", function(){
+    searchPage.classList.add("d-none")
+    iconClosePage.classList.add("d-none")
+    iconSearch.classList.remove("d-none")
+})
 
 
 
